85c9ef99e102b280450e51815184ee12eddcb48d
Jean-Michel FRANCOIS <jeanmichel.francois@qlik.com>
2025-12-31 01:25:59 +0100

    fix: folder name

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jmfrancois.github.io",
+  outDir: "./docs",
   integrations: [mdx(), sitemap()],
   vite: {
     // @ts-ignore
